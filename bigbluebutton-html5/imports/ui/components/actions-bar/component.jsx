2a5b7d49403177e699af606406f757f78d92e3c8
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-03-21 21:55:38 +0100

    remove logout button from beside plus button

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -8,58 +8,14 @@ import JoinVideoOptionsContainer from '../video-provider/video-button/container'
 import CaptionsButtonContainer from '/imports/ui/components/actions-bar/captions/container';
 import PresentationOptionsContainer from './presentation-options/component';
 import { ACTIONSBAR_HEIGHT } from '/imports/ui/components/layout/layout-manager';
-import Button from '/imports/ui/components/button/component';
-import { makeCall } from '/imports/ui/services/api';
 import PropTypes from 'prop-types';
-import { defineMessages } from 'react-intl';
-import _ from 'lodash';
-import EndMeetingConfirmationContainer from '/imports/ui/components/end-meeting-confirmation/container';
-import UserOptionsContainer from "../user-list/user-list-content/user-participants/user-options/container";
-import UserParticipants from "../user-list/user-list-content/user-participants/component";
 
 const propTypes = {
   intl: PropTypes.object.isRequired,
 };
 
 
-const intlMessages = defineMessages({
-  selectleaveSessionLabel: {
-    id: 'app.navBar.settingsDropdown.leaveSessionLabel',
-    description: 'Leave session button label',
-  },
-  selectleaveSessionDesc: {
-    id: 'app.navBar.settingsDropdown.leaveSessionDesc',
-    description: 'Describes leave session option',
-  },
-  endMeetingLabel: {
-    id: 'app.navBar.settingsDropdown.endMeetingLabel',
-    description: 'End meeting options label',
-  },
-  endMeetingDesc: {
-    id: 'app.navBar.settingsDropdown.endMeetingDesc',
-    description: 'Describes settings option closing the current meeting',
-  },
-});
-
 class ActionsBar extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.selectLeaveMeeting = _.uniqueId('action-item-');
-    this.endLeaveMeeting = _.uniqueId('action-item-');
-    // Set the logout code to 680 because it's not a real code and can be matched on the other side
-    this.LOGOUT_CODE = '680';
-
-    this.leaveSession = this.leaveSession.bind(this);
-  }
-
-  leaveSession() {
-    makeCall('userLeftMeeting');
-    // we don't check askForFeedbackOnLogout here,
-    // it is checked in meeting-ended component
-    Session.set('codeError', this.LOGOUT_CODE);
-    // mountModal(<MeetingEndedComponent code={LOGOUT_CODE} />);
-  }
   render() {
     const {
       amIPresenter,
@@ -112,31 +68,7 @@ class ActionsBar extends PureComponent {
             : null
           }
         </div>
-        <div>
-          <Button
-              hideLabel
-              label={intl.formatMessage(intlMessages.selectleaveSessionLabel)}
-              description={intl.formatMessage(intlMessages.selectleaveSessionDesc)}
-              icon="logout"
-              color="danger"
-              size="lg"
-              circle
-              onClick={() => this.leaveSession()}
-          />
 
-        </div>
-        {/*<div>*/}
-        {/*  {currentUser.role === ROLE_MODERATOR*/}
-        {/*      ? (*/}
-        {/*          <UserOptionsContainer {...{*/}
-        {/*            users,*/}
-        {/*            setEmojiStatus,*/}
-        {/*            meetingIsBreakout,*/}
-        {/*          }}*/}
-        {/*          />*/}
-        {/*      ) : null*/}
-        {/*  }*/}
-        {/*</div>*/}
         <div className={cx(actionBarClasses)}>
           <AudioControlsContainer />
           {enableVideo
