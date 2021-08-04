4f67cd5f78ac9a4906cff85dc4a68c468eb1b348
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-08-04 16:20:49 +0100

    fixing

--- bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
+++ bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
@@ -4,7 +4,12 @@ import { styles } from './styles.scss';
 const Url = "/room/resources/images/animation_500_krj475tn.gif";
 const LoadingScreen = ({ children }) => (
   <div className={styles.background}>
-      <img src={Url} alt="loading logo" style={{width: 300,height: "auto"}} />
+      {/*<img src={Url} alt="loading logo" style={{width: 300,height: "auto"}} />*/}
+      <div className={styles.spinner}>
+          <div className={styles.bounce1} />
+          <div className={styles.bounce2} />
+          <div />
+      </div>
     <div className={styles.message}>
       {children}
     </div>
