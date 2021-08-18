d2096a83320b92ab7855a8630cb3c3ba181bef9f
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-08-18 08:28:33 +0100

    push changes

--- bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
+++ bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {styles} from './styles.scss';
-import Button from "../button/component";
+import Button from '../button/component';
 
-const Url = "/room/resources/images/animation_500_krj475tn.gif";
+const Url = '/room/resources/images/animation_500_krj475tn.gif';
 const LoadingScreen = ({children}) => (
     <div className={styles.background}>
         <div className={styles.spinner}>
-            <img src={Url} alt="loading logo" style={{width: 300, height: "auto"}}/>
+            <img src={Url} alt="loading logo" style={{width: 300, height: 'auto'}}/>
             <div className={styles.bounce1}/>
             <div className={styles.bounce2}/>
             <div/>
@@ -22,7 +22,7 @@ const LoadingScreen = ({children}) => (
                 }}
             />
         </div>
-  </div>
+    </div>
 );
 
 export default LoadingScreen;
