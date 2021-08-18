6ce63b8ac15df6f208822377cd2245e19fd9682f
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-08-18 08:24:10 +0100

    push changes

--- bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
+++ bigbluebutton-html5/imports/ui/components/loading-screen/component.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { styles } from './styles.scss';
+import {styles} from './styles.scss';
+import Button from "../button/component";
 
 const Url = "/room/resources/images/animation_500_krj475tn.gif";
-const LoadingScreen = ({ children }) => (
-  <div className={styles.background}>
-      {/*<img src={Url} alt="loading logo" style={{width: 300,height: "auto"}} />*/}
-      <div className={styles.spinner}>
-          <div className={styles.bounce1} />
-          <div className={styles.bounce2} />
-          <div />
-      </div>
-    <div className={styles.message}>
-      {children}
-    </div>
+const LoadingScreen = ({children}) => (
+    <div className={styles.background}>
+        <div className={styles.spinner}>
+            <img src={Url} alt="loading logo" style={{width: 300, height: "auto"}}/>
+            <div className={styles.bounce1}/>
+            <div className={styles.bounce2}/>
+            <div/>
+        </div>
+        <div className={styles.message}>
+            {children}
+
+            <Button
+                label="Reload"
+                size="lg"
+                onClick={() => {
+                    window.location.reload();
+                }}
+            />
+        </div>
   </div>
 );
 
