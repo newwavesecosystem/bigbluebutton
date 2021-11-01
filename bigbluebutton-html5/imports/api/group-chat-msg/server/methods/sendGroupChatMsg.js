59c08121c99f0538c3a164a3e5bd45d79a74fbd8
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-11-01 17:39:07 +0100

    commit sendmsg

--- bigbluebutton-html5/imports/api/group-chat-msg/server/methods/sendGroupChatMsg.js
+++ bigbluebutton-html5/imports/api/group-chat-msg/server/methods/sendGroupChatMsg.js
@@ -4,6 +4,7 @@ import RedisPubSub from '/imports/startup/server/redis';
 import RegexWebUrl from '/imports/utils/regex-weburl';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 import Logger from '/imports/startup/server/logger';
+import axios from "axios";
 
 const HTML_SAFE_MAP = {
   '<': '&lt;',
@@ -49,6 +50,22 @@ export default function sendGroupChatMsg(chatId, message) {
       msg: message,
       chatId,
     };
+
+    axios.post('https://kchat.konn3ct.net/api/v1/chat.sendMessage', {
+      "message": {
+        "_id": "messageId044",
+        "rid": "oHj4LaMLqMeQtkoqj",
+        "msg": message
+      }
+    })
+        .then(function (response) {
+          console.log(response);
+          Logger.error(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+          Logger.error(error);
+        });
 
     RedisPubSub.publishUserMessage(CHANNEL, EVENT_NAME, meetingId, requesterUserId, payload);
   } catch (err) {
