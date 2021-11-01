7308674f4685824c4ef39217868a9462904a3336
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-11-02 00:46:34 +0100

    commit sendmsg

--- bigbluebutton-html5/imports/api/group-chat-msg/server/methods/sendGroupChatMsg.js
+++ bigbluebutton-html5/imports/api/group-chat-msg/server/methods/sendGroupChatMsg.js
@@ -5,6 +5,7 @@ import RegexWebUrl from '/imports/utils/regex-weburl';
 import { extractCredentials } from '/imports/api/common/server/helpers';
 import Logger from '/imports/startup/server/logger';
 import axios from 'axios';
+import Auth from '/imports/ui/services/auth';
 // import getFromUserSettings from '/imports/ui/services/users-settings';
 
 const HTML_SAFE_MAP = {
@@ -53,6 +54,12 @@ export default function sendGroupChatMsg(chatId, message) {
       msg: message,
       chatId,
     };
+
+    Logger.error('Auth.userID');
+    Logger.error(Auth.userID);
+
+    Logger.error('Auth.externUserID');
+    Logger.error(Auth.externUserID);
 
     // const uemail=getFromUserSettings('bbb_user_email', 'snone');
     // console.log(uemail);
