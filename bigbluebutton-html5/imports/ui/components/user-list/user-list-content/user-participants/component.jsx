f4734d06a63dbcc4924d1cfa5b9d818a88e538e4
odejinmisa <odejinmisa@newwavesecosystem.com>
2023-08-07 12:48:51 +0100

    working on users option

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/component.jsx
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-content/user-participants/component.jsx
@@ -204,26 +204,38 @@ class UserParticipants extends Component {
 
     return (
       <Styled.UserListColumn data-test="userList">
+
+        {currentUser?.role === ROLE_MODERATOR
+            ? (
+                <UserOptionsContainer {...{
+                  clearAllEmojiStatus,
+                  meetingIsBreakout,
+                  isMeetingMuteOnStart,
+                }}
+                />
+            ) : null
+        }
+
         {
           !compact
-            ? (
-              <Styled.Container>
-                <Styled.SmallTitle>
-                  {intl.formatMessage(intlMessages.usersTitle)}
-                  {users.length > 0 ? ` (${users.length})` : null}
-                </Styled.SmallTitle>
-                {currentUser?.role === ROLE_MODERATOR
-                  ? (
-                    <UserOptionsContainer {...{
-                      clearAllEmojiStatus,
-                      meetingIsBreakout,
-                      isMeetingMuteOnStart,
-                    }}
-                    />
-                  ) : null
-                }
+              ? (
+                  <Styled.Container>
+                    <Styled.SmallTitle>
+                      {intl.formatMessage(intlMessages.usersTitle)}
+                      {users.length > 0 ? ` (${users.length})` : null}
+                    </Styled.SmallTitle>
+                    {/*{currentUser?.role === ROLE_MODERATOR*/}
+                    {/*  ? (*/}
+                    {/*    <UserOptionsContainer {...{*/}
+                    {/*      clearAllEmojiStatus,*/}
+                    {/*      meetingIsBreakout,*/}
+                    {/*      isMeetingMuteOnStart,*/}
+                    {/*    }}*/}
+                    {/*    />*/}
+                    {/*  ) : null*/}
+                    {/*}*/}
 
-              </Styled.Container>
+                  </Styled.Container>
             )
             : <Styled.Separator />
         }
